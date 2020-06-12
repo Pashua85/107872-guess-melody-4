1adf8b8b7b21c1f3563e7e09f3eb588be9c9e1a6
Pashua85 <pavellashuk85@gmail.com>
2020-06-12 13:17:15 +0500

    add snapshots to WelcomeScreen

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
 ReactDOM.render(
-    <App errorsAmount={3} />,
+    <App errorAmount={3} />,
     document.querySelector(`#root`)
 );
