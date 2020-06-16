7f0748458c2eac3050450e87a13ad3382a5285c9
Pashua85 <pavellashuk85@gmail.com>
2020-06-16 09:57:38 +0500

    add mock data

---
+++ src/mocks/questions.js
@@ -0,0 +1,40 @@
+const AVATAR_URL = `https://api.adorable.io/avatars/17`;
+
+export default [
+  {
+    type: `genre`,
+    question: `Выберите музыку барокко`,
+    genre: `baroque`,
+    answers: [{
+      src: `someSrc-0`,
+      genre: `baroque`
+    }, {
+      src: `someSrc-1`,
+      genre: `classical`
+    }, {
+      src: `someSrc-2`,
+      genre: `romantic`
+    }, {
+      src: `someSrc-3`,
+      genre: `minimalism`
+    }]
+  },
+  {
+    type: `artist`,
+    question: `Кто исполняет эту песню?`,
+    song: {
+      artist: `Gregory Porter`,
+      src: `someSrc-4`
+    },
+    answers: [{
+      picture: `${AVATAR_URL}/${Math.random()}`,
+      artist: `Kurt Elling`
+    }, {
+      picture: `${AVATAR_URL}/${Math.random()}`,
+      artist: `Gregory Porter`
+    }, {
+      picture: `${AVATAR_URL}/${Math.random()}`,
+      artist: `Tony Bennett`
+    }]
+  }
+];
