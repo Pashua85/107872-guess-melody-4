2b42033e569f8fe0f19535b1e0c007a350000a65
Pashua85 <pavellashuk85@gmail.com>
2020-06-16 22:08:44 +0500

    change WelcomeScreen snapshot test

--- src/components/welcome-screen/welcome-screen.test.js
+++ src/components/welcome-screen/welcome-screen.test.js
@@ -1,18 +1,34 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import {MemoryRouter} from 'react-router-dom';
+import Enzyme, {shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import WelcomeScreen from './welcome-screen';
+
+Enzyme.configure({
+  adapter: new Adapter()
+});
 
 describe(`WelcomeScreen`, () => {
   it(`should render correctly with 3 errors`, () => {
-    const tree = renderer
-      .create(<WelcomeScreen errorAmount={3} onStartClick={() => {}} />)
-      .toJSON();
-    expect(tree).toMatchSnapshot();
+    const wrapper = shallow(
+        <MemoryRouter>
+          <WelcomeScreen
+            errorAmount={3}
+            onStartClick={() => {}}
+          />
+        </MemoryRouter>
+    );
+    expect(wrapper).toMatchSnapshot();
   });
   it(`should render correctly with 2 errors`, () => {
-    const tree = renderer
-      .create(<WelcomeScreen errorAmount={2} onStartClick={() => {}} />)
-      .toJSON();
-    expect(tree).toMatchSnapshot();
+    const wrapper = shallow(
+        <MemoryRouter>
+          <WelcomeScreen
+            errorAmount={2}
+            onStartClick={() => {}}
+          />
+        </MemoryRouter>
+    );
+    expect(wrapper).toMatchSnapshot();
   });
 });
