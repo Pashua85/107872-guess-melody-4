a0790e8aa02cd286bcc42bfc8d529547e4fe67c2
Pashua85 <pavellashuk85@gmail.com>
2020-06-30 20:08:16 +0500

    connect WellcomeScreen to store

--- src/components/welcome-screen/welcome-screen.js
+++ src/components/welcome-screen/welcome-screen.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
 import {Link} from 'react-router-dom';
+import {incStepAction} from '../../reducer';
 
 const WelcomeScreen = (props) => {
-  const {errorAmount, onStartClick} = props;
+  const {mistakesLimit, onStartClick} = props;
   return <section className="welcome">
     <div className="welcome__logo">
       <img src="img/melody-logo.png" alt="Угадай мелодию" width="186" height="83" />
@@ -20,15 +22,26 @@ const WelcomeScreen = (props) => {
     <p className="welcome__text">Правила просты:</p>
     <ul className="welcome__rules-list">
       <li>Нужно ответить на все вопросы.</li>
-      <li>Можно допустить {errorAmount} ошибки.</li>
+      <li>Можно допустить {mistakesLimit} ошибки.</li>
     </ul>
     <p className="welcome__text">Удачи!</p>
   </section>;
 };
 
 WelcomeScreen.propTypes = {
-  errorAmount: PropTypes.number.isRequired,
+  mistakesLimit: PropTypes.number.isRequired,
   onStartClick: PropTypes.func.isRequired
 };
 
-export default WelcomeScreen;
+const mapStateToProps = (state) => ({
+  mistakesLimit: state.mistakesLimit
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  onStartClick: () => (
+    dispatch(incStepAction())
+  )
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(WelcomeScreen);
+export {WelcomeScreen};
