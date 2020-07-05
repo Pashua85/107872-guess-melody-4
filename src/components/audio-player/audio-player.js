c1217e1941a8e47bfdf1b32909420fdf37ac6a4e
Pashua85 <pavellashuk85@gmail.com>
2020-07-05 16:12:44 +0500

    rewrite AudioPlayer with hook

--- src/components/audio-player/audio-player.js
+++ src/components/audio-player/audio-player.js
@@ -1,99 +1,85 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 
-class AudioPlayer extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this.getButtonClass = this.getButtonClass.bind(this);
-    this.handleTrackClick = this.handleTrackClick.bind(this);
-    this.buttonRefs = [];
+const AudioPlayer = (props) => {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [playingTrack, setPlayingTrack] = useState(null);
+  const buttonRefs = [];
 
-    this.state = {
-      isPlaying: false,
-      playingTrack: null
-    };
-  }
-
-  handleTrackClick(src, index) {
-    if (!this.state.isPlaying) {
-      this.buttonRefs[index].play();
-      this.setState({
-        isPlaying: true,
-        playingTrack: src
-      });
-    } else if (this.state.playingTrack === src) {
-      this.buttonRefs[index].pause();
-      this.setState({
-        isPlaying: false,
-        playingTrack: null,
-      });
+  const handleTrackClick = (src, index) => {
+    if (!isPlaying) {
+      buttonRefs[index].play();
+      setIsPlaying(true);
+      setPlayingTrack(src);
+    } else if (playingTrack === src) {
+      buttonRefs[index].pause();
+      setIsPlaying(false);
+      setPlayingTrack(null);
     } else {
-      this.buttonRefs.forEach((ref) => {
+      buttonRefs.forEach((ref) => {
         ref.pause();
       });
-      this.buttonRefs[index].play();
-      this.setState({
-        playingTrack: src
-      });
+      buttonRefs[index].play();
+      setPlayingTrack(src);
     }
-  }
+  };
 
-  getButtonClass(src) {
-    if (this.state.playingTrack === src) {
+  const getButtonClass = (src) => {
+    if (playingTrack === src) {
       return `track__button track__button--pause`;
     } else {
       return `track__button track__button--play`;
     }
-  }
+  };
 
-  render() {
-    const {tracks, type, onInputChange} = this.props;
+  const {tracks, type, onInputChange} = props;
 
-    return (
-      <React.Fragment>
-        {
-          tracks.map((track, index) => (
-            <div className="track" key={track.src}>
-              <button
-                className={this.getButtonClass(track.src)}
-                type="button"
-                onClick={() => {
-                  this.handleTrackClick(track.src, index);
+  return (
+    <React.Fragment>
+      {
+        tracks.map((track, index) => (
+          <div className="track" key={track.src}>
+            <button
+              className={getButtonClass(track.src)}
+              type="button"
+              onClick={() => {
+                handleTrackClick(track.src, index);
+              }}
+            >
+            </button>
+            <div className="track__status">
+              <audio
+                src={track.src}
+                ref={(btnRef) => {
+                  buttonRefs[index] = btnRef;
                 }}
               >
-              </button>
-              <div className="track__status">
-                <audio
-                  src={track.src}
-                  ref={(btnRef) => {
-                    this.buttonRefs[index] = btnRef;
-                  }}
-                >
-                </audio>
-              </div>
-              {
-                type === `genre` &&
-                <div className="game__answer">
-                  <input
-                    className="game__input visually-hidden"
-                    type="checkbox"
-                    name="answer"
-                    value={track.genre}
-                    id={track.genre}
-                    onChange={() => {
-                      onInputChange(index);
-                    }}
-                  />
-                  <label className="game__check" htmlFor={track.genre}>Отметить</label>
-                </div>
-              }
+              </audio>
             </div>
-          ))
-        }
-      </React.Fragment>
-    );
-  }
-}
+            {
+              type === `genre` &&
+              <div className="game__answer">
+                <input
+                  className="game__input visually-hidden"
+                  type="checkbox"
+                  name="answer"
+                  value={track.genre}
+                  id={track.genre}
+                  onChange={() => {
+                    onInputChange(index);
+                  }}
+                />
+                <label className="game__check" htmlFor={track.genre}>Отметить</label>
+              </div>
+            }
+          </div>
+        ))
+      }
+    </React.Fragment>
+  );
+
+
+};
 
 AudioPlayer.propTypes = {
   tracks: PropTypes.arrayOf(
